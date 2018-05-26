6abd0e4197d5029416ce81b098bd1baa88e50f14
Nate Denys <nate_denys@yahoo.com>
2018-05-26 14:47:12 -0700

    build note-count-component utilizing the created lifecycle hook

--- app/main.js
+++ app/main.js
@@ -23,7 +23,20 @@ const inputComponent = {
       this.input = '';
     }
   }
-}
+};
+
+const noteCountComponent = {
+  template: 
+  `<div class="note-count">Note count: <strong>{{ noteCount }}</strong></div>`,
+  data () {
+    return {
+      noteCount: 0
+    };
+  },
+  created() {
+    EventBus.$on('add-note', event => this.noteCount++);
+  }
+};
 
 new Vue({
   el: '#app',
@@ -34,7 +47,8 @@ new Vue({
     placeholder: 'Enter a note'
   },
   components: {
-    'input-component': inputComponent
+    'input-component': inputComponent,
+    'note-count-component': noteCountComponent
   },
   methods: {
     addNote(event) {
